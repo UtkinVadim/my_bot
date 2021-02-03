c7c57f4456e314bef2c8043fa4005c472b82940b
Vadim <utkinvadim1991@gmail.com>
2021-02-03 22:19:44 +0300

    bot v0.1

---
+++ main.py
@@ -0,0 +1,16 @@
+import telebot
+
+bot = telebot.TeleBot('1647721344:AAFsO8PS_4tR8m09oET42dcuawVVu2Blo3E')
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, сейчас я расскажу тебе гороскоп на сегодня.")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши Привет")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+
+
+bot.polling(none_stop=True, interval=0)
